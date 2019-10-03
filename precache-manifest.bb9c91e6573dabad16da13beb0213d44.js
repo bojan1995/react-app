438f134449c61683b22e3a94e4e82b5a8a5fdd18
bojan <nikolovski.bojan@students.finki.ukim.mk>
2019-10-03 13:52:45 +0200

    Updates

---
+++ precache-manifest.bb9c91e6573dabad16da13beb0213d44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c131086e28024c781c76faf10d66e5c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "ff6b71c38a93ecddbeaa",
+    "url": "/react-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "b22fe885f96472319b1e",
+    "url": "/react-app/static/css/main.8acf597f.chunk.css"
+  },
+  {
+    "revision": "ff6b71c38a93ecddbeaa",
+    "url": "/react-app/static/js/2.fbf7cc5d.chunk.js"
+  },
+  {
+    "revision": "b22fe885f96472319b1e",
+    "url": "/react-app/static/js/main.30935c5a.chunk.js"
+  },
+  {
+    "revision": "e43d239ba24783b20a9c",
+    "url": "/react-app/static/js/runtime-main.b965bb7d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app/static/media/logo.25bf045c.svg"
+  }
+]);
